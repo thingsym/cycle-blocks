08d54280ed5b75442b29368db9b4c4b452d718e9
thingsym <thingsym@gmail.com>
2022-09-09 16:02:18 +0900

    pref: add Core class Cycle_Blocks

---
+++ src/js/blocks.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/**
+ * WordPress dependencies
+ */
+import {
+	registerBlockType,
+	unstable__bootstrapServerSideBlockDefinitions,
+} from '@wordpress/blocks';
+import { __, _x } from '@wordpress/i18n';
+
+/**
+ * Function to register an individual block.
+ *
+ * @param {Object} block The block to be registered.
+ *
+ */
+const registerBlock = ( block ) => {
+	if ( ! block ) {
+		return;
+	}
+
+	let { metadata, settings, name } = block;
+
+	if ( metadata ) {
+		// for ServerSide Blocks
+		unstable__bootstrapServerSideBlockDefinitions({ [name]: metadata });
+	}
+
+	[ metadata, settings ] = applyTextdomainMetadata( metadata, settings );
+
+	registerBlockType( { name, ...metadata }, settings );
+};
+
+const applyTextdomainMetadata = ( metadata, settings ) => {
+	if ( metadata ) {
+		if ( !! metadata.title ) {
+			metadata.title = _x( metadata.title, 'block title', 'cycle-blocks' );
+			settings.title = metadata.title;
+		}
+		if ( !! metadata.description ) {
+			metadata.description = _x( metadata.description, 'block description', 'cycle-blocks' );
+			settings.description = metadata.description;
+		}
+		if ( !! metadata.keywords ) {
+			metadata.keywords = __( metadata.keywords, 'cycle-blocks' );
+			settings.keywords = metadata.keywords;
+		}
+	}
+
+	return [ metadata, settings ];
+}
+
+[
+	// Common blocks are grouped at the top to prioritize their display
+	// in various contexts — like the inserter and auto-complete components.
+].forEach( registerBlock );
